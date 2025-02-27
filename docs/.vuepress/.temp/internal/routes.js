fa83ab41c38bf4d217ef165832611484b5b437dd
“snowycn324” <snowycn324@gmai.com>
2025-02-27 19:47:41 +0800

    docs: update docs

--- docs/.vuepress/.temp/internal/routes.js
+++ docs/.vuepress/.temp/internal/routes.js
@@ -2,7 +2,7 @@ export const redirects = JSON.parse("{}")
 
 export const routes = Object.fromEntries([
   ["/", { loader: () => import(/* webpackChunkName: "index.html" */"/Users/SnowyCN/Documents/Projects/rtx-icons/docs/.vuepress/.temp/pages/index.html.js"), meta: {"title":""} }],
-  ["/icons/", { loader: () => import(/* webpackChunkName: "icons_index.html" */"/Users/SnowyCN/Documents/Projects/rtx-icons/docs/.vuepress/.temp/pages/icons/index.html.js"), meta: {"title":""} }],
   ["/usage/", { loader: () => import(/* webpackChunkName: "usage_index.html" */"/Users/SnowyCN/Documents/Projects/rtx-icons/docs/.vuepress/.temp/pages/usage/index.html.js"), meta: {"title":""} }],
+  ["/icons/", { loader: () => import(/* webpackChunkName: "icons_index.html" */"/Users/SnowyCN/Documents/Projects/rtx-icons/docs/.vuepress/.temp/pages/icons/index.html.js"), meta: {"title":""} }],
   ["/404.html", { loader: () => import(/* webpackChunkName: "404.html" */"/Users/SnowyCN/Documents/Projects/rtx-icons/docs/.vuepress/.temp/pages/404.html.js"), meta: {"title":""} }],
 ]);
